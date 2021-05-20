1c9ac21e58b964477ea5a4c80270cf153a425fbb
Daniel.Frisinghelli <daniel.frisinghelli@eurac.edu>
2021-05-20 17:57:53 +0200

    Remove deprecated documentation.

--- setup.py
+++ setup.py
@@ -222,10 +222,6 @@ DATA_FILES = {}
 # "scripts" keyword. Entry points provide cross-platform support and allow
 # `pip` to create the appropriate form of executable for the target
 # platform.
-#
-# For example, the following would provide a command called `pylandsat`
-# which executes the function `pylandsat` from this package's cli.py
-# module when invoked:
 ENTRY_POINTS = {}
 
 
